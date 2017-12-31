211953396525d2bf082b3487b3a38a8a02101011
Heather Park <hthr.prk@gmail.com>
2017-12-30 16:44:27 -0800

    Add Map component setup

---
+++ app/components/Map/index.js
@@ -0,0 +1,23 @@
+/**
+*
+* Map
+*
+*/
+
+import React from 'react';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+Map.propTypes = {
+
+};
+
+export default Map;
