a518c8ca37fa8fce2e51d17244c8ae7573ccfb88
Heather Park <hthr.prk@gmail.com>
2018-01-03 18:40:55 -0800

    Flesh out Map component

--- app/containers/Map/index.js
+++ app/containers/Map/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { geoMercator, geoPath } from 'd3-geo';
+import { feature } from 'topojson-client';
+import topoJSON from '../../data/world-map';
 
 export default function Map() {
+  const projection = geoMercator();
+  const pathGenerator = geoPath().projection(projection);
+  const features = feature(topoJSON, topoJSON.objects.countries).features;
+
+  const countries = features.map((feature, index) =>
+    (<path
+      key={`path${index}`}
+      d={pathGenerator(feature)}
+      className='countries'
+    />)
+  );
+
   return (
-    <svg></svg>
+    <svg width={10000} height={1000}>
+      {countries}
+    </svg>
   );
 }
