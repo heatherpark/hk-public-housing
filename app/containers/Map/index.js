97f4407a79deda33d177f3002fc82b21f7076ccb
Heather Park <hthr.prk@gmail.com>
2018-01-08 14:24:39 -0800

    Convert in-component properties to passed in properties

--- app/containers/Map/index.js
+++ app/containers/Map/index.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { geoMercator, geoPath } from 'd3-geo';
-import { feature } from 'topojson-client';
-import topoJSON from '../../data/hong-kong.topo';
+import { geoPath } from 'd3-geo';
 import Marker from '../Marker';
 
-export default function Map() {
-  const width = 700;
-  const height = 580;
-  const projection = geoMercator().rotate([-114.1095, -22.3964]).scale(70000).translate([width / 2, height / 2]);
-  const pathGenerator = geoPath().projection(projection);
-  const features = feature(topoJSON, topoJSON.objects['hong-kong']).features;
-  const pathStyles = {
-    fill: 'white',
-    stroke: 'black',
-    strokeWidth: 0.5
-  };
-
-  const countries = features.map((feature, index) =>
+export default function Map(props) {
+  const pathGenerator = geoPath().projection(props.projection);
+  const countries = props.topoJSONFeatures.map((feature, index) =>
     (<path
       key={`path${index}`}
       d={pathGenerator(feature)}
-      className='countries'
-      style={pathStyles}
+      className='country'
+      style={props.pathStyles}
     />)
   );
 
   return (
-    <svg width={width} height={height}>
+    <svg width={props.width} height={props.height}>
       {countries}
-      <Marker />
+      {/* <Marker /> */}
     </svg>
   );
 }
