1376e4aad95a7cc55fa3964f548255067db32db0
Heather Park <hthr.prk@gmail.com>
2018-01-08 14:55:34 -0800

    Make props for Marker component

--- app/containers/Marker/index.js
+++ app/containers/Marker/index.js
@@ -5,28 +5,16 @@
  */
 
 import React from 'react';
-import { geoMercator, geoPath } from 'd3-geo';
 
-function Marker() {
-  const width = 700;
-  const height = 580;
-  const coords = [ 114.12577, 22.49311 ];
-  const feature = { "type": "Feature", "id": 1, "geometry": { "type": "Point", "coordinates": [ 114.12577, 22.49311 ] } }
-  const projection = geoMercator().rotate([-114.1095, -22.3964]).scale(70000).translate([width / 2, height / 2]);
-  const pathGenerator = geoPath().projection(projection);
+function Marker(props) {
+  const { longitude, latitude } = props.data;
+  const coordinates = [longitude, latitude];
+
   return (
     <circle
-      cx={projection(coords)[0]}
-      cy={projection(coords)[1]}
-      r="10px"
-      fill="black"
-      stroke="black" />
-    // <path
-    //   d={pathGenerator(feature)}
-    //   className="marker"
-    //   fill="black"
-    //   stroke="black"
-    // />
+      cx={props.projection(coordinates)[0]}
+      cy={props.projection(coordinates)[1]}
+      style={props.styles} />
   );
 }
 
