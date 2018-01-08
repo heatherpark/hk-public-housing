87d3175b8121351cd1db725304fe72d60a1e4716
Heather Park <hthr.prk@gmail.com>
2018-01-08 14:55:00 -0800

    Add and propagate Marker styles

--- app/containers/MapContainer/index.js
+++ app/containers/MapContainer/index.js
@@ -9,16 +9,25 @@ import { feature } from 'topojson-client';
 import { geoMercator } from 'd3-geo';
 import Map from '../Map';
 import topoJSON from '../../data/hong-kong.topo';
+import markerData from '../../data/public-housing-marker-data';
 
 export default function MapContainer() {
   const mapWidth = 800;
   const mapHeight = 680;
-  const projection = geoMercator().rotate([-114.1095, -22.3964]).scale(70000).translate([mapWidth / 2, mapHeight / 2]);
+  const projection = geoMercator()
+    .rotate([-114.1095, -22.3964])
+    .scale(70000)
+    .translate([mapWidth / 2, mapHeight / 2]);
   const topoJSONFeatures = feature(topoJSON, topoJSON.objects['hong-kong']).features;
-  const pathStyles = {
+  const countryStyles = {
     fill: 'white',
     stroke: 'black',
     strokeWidth: 0.5
+  };
+  const markerStyles = {
+    fill: 'black',
+    stroke: 'black',
+    r: 1
   };
 
   return (
@@ -28,7 +37,9 @@ export default function MapContainer() {
         height={mapHeight}
         projection={projection} 
         topoJSONFeatures={topoJSONFeatures}
-        pathStyles={pathStyles} />
+        countryStyles={countryStyles}
+        markerData={markerData}
+        markerStyles={markerStyles} />
     </div>
   );
 }
