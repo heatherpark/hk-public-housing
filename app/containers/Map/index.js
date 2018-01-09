f70eb011b53fe36368af66c9edee10d597228d5e
Heather Park <hthr.prk@gmail.com>
2018-01-09 13:27:11 -0800

    Move state to MapContainer

--- app/containers/Map/index.js
+++ app/containers/Map/index.js
@@ -1,76 +1,27 @@
 import React from 'react';
 import { geoPath } from 'd3-geo';
 import './Map.css';
-import Marker from '../Marker';
-import Tooltip from '../../components/Tooltip';
 
-export default class Map extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Map(props) {
+  const pathGenerator = geoPath()
+    .projection(props.projection);
 
-    this.state = {
-      showTooltip: false,
-      tooltipData: {},
-      hoverPosition: []
-    };
-
-    this.handleMarkerMouseOver = this.handleMarkerMouseOver.bind(this);
-    this.handleMarkerMouseOut = this.handleMarkerMouseOut.bind(this);
-  }
-
-  handleMarkerMouseOver(event, data) {
-    event.persist();
-
-    setTimeout(() => {
-      this.setState({ 
-        showTooltip: true,
-        tooltipData: data,
-        hoverPosition: [event.clientX, event.clientY]
-      });
-    }, 300);
-  }
-
-  handleMarkerMouseOut(event) {
-    this.setState({ showTooltip: false });
-  }
-
-  render() {
-    const pathGenerator = geoPath()
-      .projection(this.props.projection);
-
-    const countries = this.props.topoJSONFeatures.map((feature, index) => {
+  const countries = props.topoJSONFeatures
+    .map((feature, index) => {
       return (
         <path
+          // TODO: update key value
           key={`path${index}`}
           d={pathGenerator(feature)}
-          className='country'
+          className="country"
         />
       );
-    });
+  });
 
-    const markers = this.props.markerData &&
-      this.props.markerData.map(data => {
-        return (
-          <Marker
-            data={data}
-            projection={this.props.projection}
-            radius={this.props.markerRadius}
-            handleMouseOver={this.handleMarkerMouseOver}
-            handleMouseOut={this.handleMarkerMouseOut} />
-        );
-      });
-
-    return (
-      <div className="map">
-        <Tooltip
-          data={this.state.tooltipData}
-          position={this.state.hoverPosition}
-          opacity={this.state.showTooltip ? 1 : 0} />
-        <svg viewBox={`0 0 ${this.props.width} ${this.props.height}`}>
-          {countries}
-          {this.props.markerData && markers}
-        </svg>
-      </div>
-    );
-  }
+  return (
+    <svg viewBox={`0 0 ${props.width} ${props.height}`}>
+      {countries}
+      {props.children}
+    </svg>
+  );
 }
--- app/containers/MapContainer/index.js
+++ app/containers/MapContainer/index.js
@@ -1,37 +1,89 @@
-/**
- *
- * MapContainer
- *
- */
-
 import React from 'react';
 import { feature } from 'topojson-client';
 import { geoMercator } from 'd3-geo';
+
 import Map from '../Map';
+import '../Map/Map.css';
 import topoJSON from '../../data/hong-kong.topo';
+
+import Tooltip from '../../components/Tooltip';
+
+import Marker from '../Marker';
 import markerData from '../../data/public-housing-marker-data';
 
-export default function MapContainer() {
-  const mapWidth = 600;
-  const mapHeight = 480;
+export default class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const projection = geoMercator()  // projection type
-    .center([114.1095, 22.3964])  // set center of projection to Hong Kong coordinates
-    .scale(50000)
-    .translate([mapWidth / 2, mapHeight / 3]);
+    this.state = {
+      showTooltip: false,
+      tooltipData: {},
+      hoverPosition: []
+    };
 
-  const geoJSONObjectKey = 'hong-kong';
-  const topoJSONFeatures = feature(topoJSON, topoJSON.objects[geoJSONObjectKey]).features;
+    this.handleMarkerMouseOver = this.handleMarkerMouseOver.bind(this);
+    this.handleMarkerMouseOut = this.handleMarkerMouseOut.bind(this);
+  }
 
-  return (
-    <div>
-      <Map
-        width={mapWidth}
-        height={mapHeight}
-        projection={projection}
-        topoJSONFeatures={topoJSONFeatures}
-        markerData={markerData}
-        markerRadius={8} />
-    </div>
-  );
+  handleMarkerMouseOver(event, data) {
+    event.persist();
+
+    setTimeout(() => {
+      this.setState({ 
+        showTooltip: true,
+        tooltipData: data,
+        hoverPosition: [event.clientX, event.clientY]
+      });
+    }, 300);
+  }
+
+  handleMarkerMouseOut(event) {
+    setTimeout(() => {
+      this.setState({ showTooltip: false });
+    }, 300);
+  }
+
+  render() {
+    const mapWidth = 600;
+    const mapHeight = 480;
+
+    const projection = geoMercator()  // projection type
+      .center([114.1095, 22.3964])  // set center of projection to Hong Kong coordinates
+      .scale(50000)
+      .translate([mapWidth / 2, mapHeight / 3]);
+
+    const geoJSONObjectKey = 'hong-kong';
+    const topoJSONFeatures = feature(topoJSON, topoJSON.objects[geoJSONObjectKey]).features;
+
+    const markers = markerData &&
+      markerData.map(data => {
+        return (
+          <Marker
+            data={data}
+            projection={projection}
+            radius={9}
+            handleMouseOver={this.handleMarkerMouseOver}
+            handleMouseOut={this.handleMarkerMouseOut} />
+        );
+      });
+
+    return (
+      <div className="map">
+        <Tooltip  
+          data={this.state.tooltipData}
+          position={this.state.hoverPosition}
+          opacity={this.state.showTooltip ? 1 : 0} />
+        <Map
+          width={mapWidth}
+          height={mapHeight}
+          projection={projection}
+          topoJSONFeatures={topoJSONFeatures}
+          markerData={markerData}
+          markerRadius={8}>
+          {/* only map-related SVG elements should be children of Map */}
+          {markers}
+        </Map>
+      </div>
+    );
+  }
 }
