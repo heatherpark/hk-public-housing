0ad68b2c43cd872e115ebb42f399f9938ca5af70
Heather Park <hthr.prk@gmail.com>
2018-01-09 11:00:18 -0800

    Organize and comment code

--- app/containers/MapContainer/index.js
+++ app/containers/MapContainer/index.js
@@ -11,23 +11,24 @@ import Map from '../Map';
 import topoJSON from '../../data/hong-kong.topo';
 import markerData from '../../data/public-housing-marker-data';
 
-const mapWidth = 600;
-const mapHeight = 480;
-
-const projection = geoMercator()
-  .center([114.1095, 22.3964])
-  .scale(50000)
-  .translate([mapWidth / 2, mapHeight / 3]);
-
-const topoJSONFeatures = feature(topoJSON, topoJSON.objects['hong-kong']).features;
-
 export default function MapContainer() {
+  const mapWidth = 600;
+  const mapHeight = 480;
+
+  const projection = geoMercator()  // projection type
+    .center([114.1095, 22.3964])  // set center of projection to Hong Kong coordinates
+    .scale(50000)
+    .translate([mapWidth / 2, mapHeight / 3]);
+
+  const geoJSONObjectKey = 'hong-kong';
+  const topoJSONFeatures = feature(topoJSON, topoJSON.objects[geoJSONObjectKey]).features;
+
   return (
     <div>
       <Map
         width={mapWidth}
         height={mapHeight}
-        projection={projection} 
+        projection={projection}
         topoJSONFeatures={topoJSONFeatures}
         markerData={markerData}
         markerRadius={8} />
