b080e05e286b773b15f7245e0ae6b1d538ddcdf1
Heather Park <hthr.prk@gmail.com>
2018-01-10 11:34:55 -0800

    Create and incorporate accent bar

---
+++ src/components/AccentBar/AccentBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './AccentBar.css';
+
+// TODO:  Make component reusable
+export default function AccentBar() {
+  return (
+    <div className="accent-bar"></div>
+  );
+}
--- src/components/MapPage/Intro/MapIntro.jsx
+++ src/components/MapPage/Intro/MapIntro.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './MapIntro.css';
 import MapHeading from '../Heading/MapHeading';
 import MapDescription from '../Description/MapDescription';
+import AccentBar from '../../AccentBar/AccentBar';
 
 export default function MapIntro() {
   return (
     <div className="map-intro">
       <MapHeading />
+      <AccentBar />
       <MapDescription />
     </div>
   );
