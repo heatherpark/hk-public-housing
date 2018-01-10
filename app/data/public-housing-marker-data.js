61310a95045d4dbb3b70dee4792ecad880bea89d
Heather Park <hthr.prk@gmail.com>
2018-01-09 18:45:50 -0800

    Delete previous boilerplate

--- app/data/public-housing-marker-data.js
+++
@@ -1,7 +0,0 @@
-import publicHousingMarkerData from '../js/publicHousingMarkerData';
-import publicHousingDataKeyMap from '../js/publicHousingDataKeyMap';
-import publicHousingData from './public-housing';
-
-var markerData = publicHousingMarkerData(publicHousingData, publicHousingDataKeyMap);
-
-export default markerData;
