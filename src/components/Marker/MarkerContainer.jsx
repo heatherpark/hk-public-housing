6f531a40c4c55dd33b8c3235473b3d7b33e89501
Heather Park <hthr.prk@gmail.com>
2018-01-09 18:55:53 -0800

    Add Marker component directory

---
+++ src/components/Marker/MarkerContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '.Marker.css';
+import Marker from './Marker';
+
+export default function MarkerContainer(props) {
+  return (
+    <svg
+      onMouseOver={(e) => props.handleMouseOver(e, props.data)}
+      onMouseOut={(e) => props.handleMouseOut(e)}>
+      <Marker
+        projection={props.projection}
+        data={props.data}
+        radius={props.radius} />
+    </svg>
+  );
+}
---
+++ src/components/Marker/index.js
@@ -0,0 +1,2 @@
+export { default as Marker } from './Marker';
+export { default as MarkerContainer } from './MarkerContainer';
