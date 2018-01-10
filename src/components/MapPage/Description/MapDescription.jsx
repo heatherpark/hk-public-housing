96460a4bf3799550dc3277a7b1be2f88e774293b
Heather Park <hthr.prk@gmail.com>
2018-01-10 15:03:52 -0800

    Update description text

--- src/components/MapPage/Description/MapDescription.jsx
+++ src/components/MapPage/Description/MapDescription.jsx
@@ -3,14 +3,25 @@ import './MapDescription.css';
 
 export default function MapDescription() {
   return (
-    <p className="map-description">
-      Public housing in Hong Kong is a set of mass housing programmes
-      through which the Government of Hong Kong provides affordable housing for
-      lower-income residents. It is a major component of housing in Hong Kong,
-      with nearly half of the population now residing in some form of public housing.
-      The public housing policy dates to 1954, after a fire in Shek Kip Mei destroyed
-      thousands of shanty homes end of 1953 and prompted the government to begin
-      constructing homes for the poor.
+    // TODO: Provide sources
+    <div className="map-description">
+      <p>
+      Hong Kong's housing market has ranked first as the most expensive in the world
+      seven years running. The <em>Demographia International Housing Affordability</em> survey
+      uses an equation called the "Median Multiple", which is an urban market's median
+      house price divided by its median household income, to rate middle-income housing
+      affordability. When considering major housing markets (those with over 1,000,000 population),
+      Hong Kong's Median Multiple is 18.1, while the country that ranks second, New Zealand,
+      has a Median Multiple of 10.0.
     </p>
+    <p>
+      Hong Kong's government provides lower-income residents with subsidized, affordable housing.
+      Almost half of the territory's population of over 7 million lives in some form of
+      public housing. This map depicts the locations of these housing estates.  Each marker is 
+      relative in size to the number of rental units offered by a particular estate compared to
+      the total number of units offered by public housing as a whole. Hover over a marker to see 
+      more details about an estate.
+    </p>
+    </div>
   );
 }
