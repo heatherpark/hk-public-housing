9751eb97e429a47a7ce1ae437587c0a3031e958f
Heather Park <hthr.prk@gmail.com>
2018-01-10 11:07:21 -0800

    Add fade effect for markers

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -4,7 +4,7 @@ import './Marker.css';
 export default function Marker(props) {
   return (
     <circle
-      className="marker"
+      className="marker fade-in"
       cy={props.projection(props.data.coordinates)[1]}
       cx={props.projection(props.data.coordinates)[0]}
       r={props.radius} />
