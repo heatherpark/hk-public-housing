71d29fe7c395d75e1901e1a6fb7dc92e93453c99
Heather Park <hthr.prk@gmail.com>
2018-03-31 11:11:34 -0700

    Add data for median multiples

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
-export { default as topoJSON } from 'hong-kong.topo';
-export { default as markerData } from 'public-housing-marker-data';
-export { default as publicHousingData } from 'public-housing';
+export { default as topoJSON } from './hong-kong.topo';
+export { default as markerData } from './public-housing-marker-data';
+export { default as publicHousingData } from './public-housing';
+export { default as medianMultipleData } from './median-multiples';
