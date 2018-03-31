82858f82eab2dedd07022b36dabe01dc9a538694
Heather Park <hthr.prk@gmail.com>
2018-03-31 11:11:34 -0700

    Add data for median multiples

---
+++ src/data/median-multiples.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    city: "Hong Kong",
+    medianMultiple: 19.4
+  },
+
+  {
+    city: "Sydney",
+    medianMultiple: 12.9
+  },
+
+  {
+    city: "Vancouver",
+    medianMultiple: 12.6
+  },
+
+  {
+    city: "San Jose",
+    medianMultiple: 10.3
+  },
+
+  {
+    city: "Melbourne",
+    medianMultiple: 9.9
+  },
+
+  {
+    city: "Los Angeles",
+    medianMultiple: 9.4
+  },
+
+  {
+    city: "Honolulu",
+    medianMultiple: 9.2
+  },
+
+  {
+    city: "San Francisco",
+    medianMultiple: 9.1
+  },
+
+  {
+    city: "Auckland",
+    medianMultiple: 8.8
+  },
+
+  {
+    city: "London",
+    medianMultiple: 8.5
+  }
+]
